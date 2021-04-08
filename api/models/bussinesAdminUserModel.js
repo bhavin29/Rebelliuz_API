a1a1ccbfe9f68080078c8d213f24326466f35505
bhavin29 <p.bhavin29@gmail.com>
2021-04-08 17:11:33 +0530

    admin user model

---
+++ api/models/bussinesAdminUserModel.js
@@ -0,0 +1,26 @@
+'use strict';
+   
+// Import mongoose 
+const  mongoose = require("mongoose");
+const aggregatePaginate = require('mongoose-aggregate-paginate-v2');
+
+// Declare schema and assign Schema class
+const  Schema = mongoose.Schema;
+
+// Create Schema Instance for User and add properties
+const  BussinesAdminUserSchema = new  Schema({
+    bussines_id: { type: String, required:  true },
+    bussines_user_id: { type: String, required:  true },
+    role: { type: String, required:  true },
+    isactive: { type: Boolean, default: 1 },
+    created_on: { type: Date, default: Date.now },
+    created_by: { type: Number, default: 0 }
+});
+
+BussinesAdminUserSchema.plugin(aggregatePaginate);
+
+
+//role : HR, Panaliest
+
+// Create and export User model
+module.exports = mongoose.model("bussines_admin_users", BussinesAdminUserSchema);
