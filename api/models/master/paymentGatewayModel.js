132039d29937111af16552881ab4edff1788ff8d
bhavin29 <p.bhavin29@gmail.com>
2021-05-29 21:20:27 +0530

    payment model chagnes

--- api/models/master/paymentGatewayModel.js
+++ api/models/master/paymentGatewayModel.js
@@ -9,9 +9,9 @@ const  Schema = mongoose.Schema;
 // Create Schema add properties
 const  PaymentGatewaySchema = new  Schema({
     title: { type: String, unique: true, trim: true, required:  true },
-    description: { type: String, unique: true, trim: true, required:  true },
-    config: { type: Object, unique: true, trim: true, required:  true },
-    test_mode: { type: String, unique: true, trim: true, required:  true },
+    description: { type: String, trim: true, required:  true },
+    config: { type: Object,  trim: true, required:  true },
+    test_mode: { type: String, trim: true, required:  true },
     isactive: { type: Boolean, trim:true, default: 1 } ,
     created_on: { type: Date, default: Date.now },
     created_by: { type: String, default: 0 }
